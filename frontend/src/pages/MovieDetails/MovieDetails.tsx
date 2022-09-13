31589ad75612e95bec77d24b27115801ea275da7
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-09-13 15:45:45 -0300

    Criacao e estilizacao da MovieId Page

--- frontend/src/pages/MovieDetails/MovieDetails.tsx
+++ frontend/src/pages/MovieDetails/MovieDetails.tsx
@@ -1,9 +1,45 @@
 import './styles.css';
+import { ReactComponent as MainImage } from '../../assets/images/star.svg';
 
 const MovieDetails = () => {
-    return (
-        <h1>Movie Details Page</h1>
-    );
-}
+  return (
+    <div className="movie-details-container">
+      <h1>Detalhes do Filme <br /> id: 1</h1>
+      <div className="movie-details-form base-card">
+        <form action="submit" className='form-itens'>
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Deixe sua avaliação aqui"
+            name="username"
+          />
+          <button>SALVAR AVALIAÇÃO</button>
+        </form>
+      </div>
+      <div className="movie-details-evaluation base-card">
+        <div className='evaluation-username'>
+          <MainImage />
+          <h1>Nome do Usuário</h1>
+        </div>
+        <div className="evaluation">Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco.</div>
+        <div className='evaluation-username'>
+          <MainImage />
+          <h1>Nome do Usuário</h1>
+        </div>
+        <div className="evaluation">Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco.</div>
+        <div className='evaluation-username'>
+          <MainImage />
+          <h1>Nome do Usuário</h1>
+        </div>
+        <div className="evaluation">Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco.</div>
+        <div className='evaluation-username'>
+          <MainImage />
+          <h1>Nome do Usuário</h1>
+        </div>
+        <div className="evaluation">Gostei muito do filme. Foi muito bom mesmo. Pena que durou pouco.</div>
+      </div>
+    </div>
+  );
+};
 
 export default MovieDetails;
